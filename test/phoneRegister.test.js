a6b502f73994f5b444592988cc3f048eed684b6d
Suyel Bishnu <suyel2007@gmail.com>
2024-10-07 10:54:24 +0300

    Add new test

--- test/phoneRegister.test.js
+++ test/phoneRegister.test.js
@@ -2,3 +2,15 @@ import {expect} from 'chai';
 
 import PhoneRegister from '../phoneRegister.js';
 
+import {createRequire} from 'module';
+const require = createRequire(import.meta.url);
+
+const phones = require('../phones.json');
+
+describe('test getTypes', function(){
+    it('using default data', function(){
+        const register=new PhoneRegister(phones);
+        expect(register.getTypes()).to.deep.equal(['home', 'work', 'mobile']);
+    });
+});
+
